073d257c380d4bcafcb47e08aa5142ae5b06b77a
RobinCouture <rcouture1@myges.fr>
2025-01-15 23:24:07 +0100

    implemented ScrollableList

--- src/UI/TeamBuilder/CharaDisplayList.tsx
+++ src/UI/TeamBuilder/CharaDisplayList.tsx
@@ -1,20 +1,26 @@
 import { useRef, useState } from "react";
 import Character from "../../classes/Character";
-
+import CharaDisplayRow from "./CharaDisplayRow";
+import ScrollableList from "../../classes/ScrollableList";
 
 interface CharaDisplayListProps {
-    characters: Map<string, Character>;
+  characters?: Map<string, Character>;
 }
 
-
-function CharaDisplayList({characters} : CharaDisplayListProps) {
-    
-    
-    return (
-        <>  
-            <p>CharaDisplayList</p>
-        </>
-    );
+function CharaDisplayList({ characters }: CharaDisplayListProps) {
+  const numberOfItems: number = characters?.size || 0;
+  const CharaElementList = Array.from(characters?.values() || []).map(
+    (chara, index) => {
+      return <CharaDisplayRow key={index} character={chara} />;
+    }
+  );
+  return (
+    <>
+    <ScrollableList height="500px">
+        {CharaElementList}
+    </ScrollableList>
+    </>
+  );
 }
 
 export default CharaDisplayList;
