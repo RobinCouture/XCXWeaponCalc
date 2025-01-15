83ceec6d005eccf8b6bf30ba76a7207e23ba91fa
RobinCouture <rcouture1@myges.fr>
2025-01-15 23:24:29 +0100

    Alt Shift F

--- src/UI/TeamBuilder/CharaDisplayRow.tsx
+++ src/UI/TeamBuilder/CharaDisplayRow.tsx
@@ -2,19 +2,19 @@ import Character from "../../classes/Character";
 import images from "../../assets/characters/characterImageImport.tsx";
 
 interface CharaDisplayRowProps {
-    character: Character | undefined;
+  character: Character | undefined;
 }
 
-function CharaDisplayRow({character} : CharaDisplayRowProps) {
-    return (
-        <>
-            {character && (
-                <img
-                    src={images[character.name as keyof typeof images]}
-                />
-            )}
-        </>
-    )
+function CharaDisplayRow({ character }: CharaDisplayRowProps) {
+  return (
+    <>
+      <li>
+        {character && (
+          <img src={images[character.name as keyof typeof images]} />
+        )}
+      </li>
+    </>
+  );
 }
 
-export default CharaDisplayRow
+export default CharaDisplayRow;
