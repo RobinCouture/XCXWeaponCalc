e0d3379abfed0333144e57734b3228bca32a3e09
RobinCouture <147134048+RobinCouture@users.noreply.github.com>
2025-01-15 14:33:52 +0100

    random test

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import PartyMemberSelector from "./UI/PartyMemberSelector"
 import './App.css'
 import WeaponOddCalc from './Applications/WeaponOddCalc'
+import CharaDisplayRow from './UI/TeamBuilder/CharaDisplayRow'
+import images from "./assets/characters/characterImageImport.tsx";
+import Character from './classes/Character'
 
 function App() {
   const [count, setCount] = useState(0)
+  const chara : Map<string, Character> = Character.getCharacters();
 
   return (
     <>
-      <WeaponOddCalc />
+      <CharaDisplayRow character={chara.get("Elma")} />
     </>
   )
 }
