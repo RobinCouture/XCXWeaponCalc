75ae630abb346c06939e2b33fec695469df7bedc
RobinCouture <147134048+RobinCouture@users.noreply.github.com>
2025-01-16 17:00:06 +0100

    minor stuff

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import CharaDisplayRow from './UI/TeamBuilder/CharaDisplayRow'
 import images from "./assets/characters/characterImageImport.tsx";
 import Character from './classes/Character'
 import CharaDisplayList from './UI/TeamBuilder/CharaDisplayList.tsx'
+import TeamDisplayList from './UI/TeamBuilder/TeamDisplayList.tsx'
+import TeamBuilder from './Applications/TeamBuilder.tsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,8 +15,7 @@ function App() {
 
   return (
     <>
-      <h1>Test</h1>
-      <CharaDisplayList characters={chara} />
+      <TeamBuilder characters={chara}/>
     </>
   )
 }
