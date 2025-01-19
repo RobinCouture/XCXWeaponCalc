5294ce34a5c5c78a4a4a4d55b4c1aa60dd8ab4c3
RobinCouture <rcouture1@myges.fr>
2025-01-19 21:44:10 +0100

    added click reactivity on team side

--- src/Applications/TeamBuilder.tsx
+++ src/Applications/TeamBuilder.tsx
@@ -5,51 +5,71 @@ import "../style/TeamBuilder/TeamBuilder.css";
 import { useEffect, useState } from "react";
 
 interface TeamBuilderProps {
-    characters?: Map<string, Character>;
+  characters?: Map<string, Character>;
 }
 
-function TeamBuilder({characters}: TeamBuilderProps) {
-    const [emptySlot, setEmptySlot] = useState<number>(1);
-    const [team, setTeam] = useState<Character[]>([
-        new Character("Cross"),
-        Character.getBlankCharacter(),
-        Character.getBlankCharacter(),
-        Character.getBlankCharacter()
-    ]);
-    const [isTeamFull, setIsTeamFull] = useState<boolean>(emptySlot === 4);
+function TeamBuilder({ characters }: TeamBuilderProps) {
+  const [emptySlot, setEmptySlot] = useState<number>(1);
+  const [team, setTeam] = useState<Character[]>([
+    new Character("Cross"),
+    Character.getBlankCharacter(),
+    Character.getBlankCharacter(),
+    Character.getBlankCharacter(),
+  ]);
+  const [isTeamFull, setIsTeamFull] = useState<boolean>(emptySlot === 4);
 
-    useEffect(() => {
-        setIsTeamFull(emptySlot === 4);
-        console.log("Team is full:", isTeamFull);
-    }), [emptySlot];
+  useEffect(() => {
+    setIsTeamFull(emptySlot === 4);
+  }),
+    [emptySlot];
 
-    function handleCharaClicked(chara: Character, selected: Boolean) {
-        console.log(chara?.name, selected);
-        if (selected) {
-            if (emptySlot < 4) {
-                let newTeam = [...team];
-                newTeam[emptySlot] = chara;
-                setTeam(newTeam);
-                setEmptySlot(emptySlot + 1);
-            }
-        } else {
-            if (team.includes(chara)) {
-            let newTeam = team.filter((c) => c !== chara);
-            newTeam.push(Character.getBlankCharacter());
-            setTeam(newTeam);
-            setEmptySlot(emptySlot - 1);
-            }
-        }
+  function handleCharaClicked(chara: Character) {
+    console.log(chara?.name, team.includes(chara));
+    if (!team.includes(chara)) {
+      addCharaToTeam(chara);
+    } else {
+      removeCharaFromTeam(chara);
     }
+  }
 
-    return (
-        <>
-        <div>
-            <TeamDisplayList characters={team}/>
-            <CharaDisplayList characters={characters} onCharaClicked={handleCharaClicked} isTeamFull={isTeamFull}/>
-        </div>
-        </>
-    );
+  function handleTeamMemberClicked(chara: Character) {
+    if (chara.name !== "Blank") {
+      removeCharaFromTeam(chara);
+    }
+  }
+
+  function addCharaToTeam(chara: Character) {
+    if (emptySlot < 4) {
+      let newTeam = [...team];
+      newTeam[emptySlot] = chara;
+      setTeam(newTeam);
+      setEmptySlot(emptySlot + 1);
+    }
+  }
+
+  function removeCharaFromTeam(chara: Character) {
+    let newTeam = team.filter((c) => c !== chara);
+    newTeam.push(Character.getBlankCharacter());
+    setTeam(newTeam);
+    setEmptySlot(emptySlot - 1);
+    setIsTeamFull(false);
+  }
+
+  return (
+    <>
+      <div>
+        <TeamDisplayList
+          characters={team}
+          onCharaClicked={handleTeamMemberClicked}
+        />
+        <CharaDisplayList
+          characters={characters}
+          onCharaClicked={handleCharaClicked}
+          team={team}
+        />
+      </div>
+    </>
+  );
 }
 
 export default TeamBuilder;
