58d83bdbc60944e9f526f2abd12aea54f48d031a
RobinCouture <147134048+RobinCouture@users.noreply.github.com>
2025-01-29 14:15:26 +0100

    Create WeaponSelector.tsx

---
+++ src/UI/WeaponOddsFinder/WeaponSelector.tsx
@@ -0,0 +1,25 @@
+import Weapons from "../../classes/DataClass/Weapons";
+
+interface WeaponSelectorProps {
+    weapons: Map<String, Weapons>;
+    onWeaponSelection: (weapon: Weapons) => void;
+}
+
+function WeaponSelector({weapons, onWeaponSelection}: WeaponSelectorProps) {
+    const type : String = Array.from(weapons.values())[0].type;
+    const options = Array.from(weapons.values()).map((weapon) => {
+        return (
+            <option value={weapon.name}>{weapon.name}</option>
+        )
+    });
+    return (
+    <>
+        <label htmlFor="selector">Select {type} weapon</label>
+        <select name="selector" id="selector" onChange={(e) => onWeaponSelection(weapons.get(e.target.value) || new Weapons())}>
+            {options}
+        </select>
+    </>
+    )
+}
+
+export default WeaponSelector;
